a057056186778bbeec6bb80ecf52121790bac426
Sahar Tawfiq <stawfiq10@yahoo.com>
2018-05-15 23:58:46 -0400

    db changes and image folder

--- db/schema.sql
+++ db/schema.sql
@@ -3,26 +3,37 @@ USE chefs_db;
 
 CREATE TABLE chefs
 (
-	id int NOT NULL AUTO_INCREMENT,
+	chefId int(10) NOT NULL ,
 	name varchar(255) NOT NULL,
-	password varchar(255),
+	password varchar(50),
     email varchar(300),
-    recipeCount int,
-    
-	PRIMARY KEY (id)
+    recipeCount int(10),
+	PRIMARY KEY (chefId)
 );
-USE chefs_db;
 
+USE chefs_db;
 CREATE TABLE recipes
 (
-	id int NOT NULL AUTO_INCREMENT,
+	recipeId int(10) NOT NULL AUTO_INCREMENT ,
+    chefId int(10),
 	title varchar(255) NOT NULL,
-    ingredients varchar(400),
     instructions varchar(500),
     imageLink varchar(255),
-    prepTime int,
+    prepTime int(50),
     method varchar(255),
-    chefId int NOT NULL,
-	PRIMARY KEY (id)
+	PRIMARY KEY (recipeId),
+    FOREIGN Key (chefId) REFERENCES chefs (ChefId)
+);
+
+USE chefs_db;
+CREATE TABLE ingredients
+(
+	id int(10) NOT NULL AUTO_INCREMENT,
+    recipeId int(10),
+    ingredient varchar(225),
+    amount DECIMAL(5,2),
+    measurement varchar(100),
+	PRIMARY KEY (id),
+    FOREIGN KEY (recipeId) REFERENCES recipes (recipeId)
 );
 
---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO chefs (chefId, name, email, password,recipeCount) VALUES (1, "sahar",  "sahar@gmail.com","aaaa", 2);
+INSERT INTO chefs (chefId, name, email, password,recipeCount) VALUES (2, "ali", "ali@gmail.com","3333", 1);
+INSERT INTO chefs (chefId, name, email, password,recipeCount) VALUES (3, "Susan",  "susan@gmail.com","1515", 1);
+
+
+INSERT INTO recipes (chefId, title, instructions, imageLink, prepTime, method) VALUES (1, "chicken currey",  "In a large pot fry the onions till add the chicken potato and curry powder and cook for five minutes add the stoch and let it simmer for 30 minutes", "/images/chickenPotatoCurry.jpg", 15, "stove");
+INSERT INTO recipes (chefId, title, instructions, imageLink, prepTime, method) VALUES (2, "Date Balls", "Mix all ingredients in a food processor till it form a homogenous mix shape into 3/4 of an inch balls"," -", 30, "non");
+
+INSERT INTO ingredients (recipeId, ingredient, amount, measurement) VALUES (1, "chicken breasts", 2, "lb");
+INSERT INTO ingredients (recipeId, ingredient, amount, measurement) VALUES (1, "cubed potato", 1, "lb" );
+INSERT INTO ingredients (recipeId, ingredient, amount, measurement) VALUES (1, "curry powder" ,2 , "tablespoon");
+INSERT INTO ingredients (recipeId, ingredient, amount, measurement) VALUES (1, "oil", 2, "tablespoon");
+INSERT INTO ingredients (recipeId, ingredient, amount, measurement) VALUES (1,"salt" , 0,"to taste");
+INSERT INTO ingredients (recipeId, ingredient, amount, measurement) VALUES (2, "chopped dates", 2, "cups");
+INSERT INTO ingredients (recipeId, ingredient, amount, measurement) VALUES (2, "chopped walnuts", 1, "cups");
+INSERT INTO ingredients (recipeId, ingredient, amount, measurement) VALUES (2, "tahini", 2, "tablespoons");
