40f2f4d99425af082eb5d5714794ef9a4ad556bc
goergena <goergena@users.noreply.github.com>
2018-05-17 12:27:08 -0400

    seeds for fourth table recipeingredients

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,11 +6,20 @@ INSERT INTO chefs (name, email, password,recipeCount) VALUES ("Susan",  "susan@g
 INSERT INTO recipes (chefId, title, instructions, imageLink, prepTime, method) VALUES (1, "chicken currey",  "In a large pot fry the onions till add the chicken potato and curry powder and cook for five minutes add the stoch and let it simmer for 30 minutes", "/images/chickenPotatoCurry.jpg", 15, "stove");
 INSERT INTO recipes (chefId, title, instructions, imageLink, prepTime, method) VALUES (2, "Date Balls", "Mix all ingredients in a food processor till it form a homogenous mix shape into 3/4 of an inch balls"," -", 30, "non");
 
-INSERT INTO ingredients (recipeId, ingredient, amount, measurement) VALUES (1, "chicken breasts", 2, "lb");
-INSERT INTO ingredients (recipeId, ingredient, amount, measurement) VALUES (1, "cubed potato", 1, "lb" );
-INSERT INTO ingredients (recipeId, ingredient, amount, measurement) VALUES (1, "curry powder" ,2 , "tablespoon");
-INSERT INTO ingredients (recipeId, ingredient, amount, measurement) VALUES (1, "oil", 2, "tablespoon");
-INSERT INTO ingredients (recipeId, ingredient, amount, measurement) VALUES (1,"salt" , 0,"to taste");
-INSERT INTO ingredients (recipeId, ingredient, amount, measurement) VALUES (2, "chopped dates", 2, "cups");
-INSERT INTO ingredients (recipeId, ingredient, amount, measurement) VALUES (2, "chopped walnuts", 1, "cups");
-INSERT INTO ingredients (recipeId, ingredient, amount, measurement) VALUES (2, "tahini", 2, "tablespoons");
+INSERT INTO ingredients (ingredient, amount, measurement) VALUES ("chicken breasts", 2, "lb");
+INSERT INTO ingredients (ingredient, amount, measurement) VALUES ("cubed potato", 1, "lb" );
+INSERT INTO ingredients (ingredient, amount, measurement) VALUES ("curry powder" ,2 , "tablespoon");
+INSERT INTO ingredients (ingredient, amount, measurement) VALUES ("oil", 2, "tablespoon");
+INSERT INTO ingredients (ingredient, amount, measurement) VALUES ("salt" , 0,"to taste");
+INSERT INTO ingredients (ingredient, amount, measurement) VALUES ("chopped dates", 2, "cups");
+INSERT INTO ingredients (ingredient, amount, measurement) VALUES ("chopped walnuts", 1, "cups");
+INSERT INTO ingredients (ingredient, amount, measurement) VALUES ("tahini", 2, "tablespoons");
+
+INSERT INTO recipeingredients (ingredientId, recipeId) VALUES (1, 1);
+INSERT INTO recipeingredients (ingredientId, recipeId) VALUES (2, 1);
+INSERT INTO recipeingredients (ingredientId, recipeId) VALUES (3, 1);
+INSERT INTO recipeingredients (ingredientId, recipeId) VALUES (4, 1);
+INSERT INTO recipeingredients (ingredientId, recipeId) VALUES (5, 1);
+INSERT INTO recipeingredients (ingredientId, recipeId) VALUES (6, 2);
+INSERT INTO recipeingredients (ingredientId, recipeId) VALUES (7, 2);
+INSERT INTO recipeingredients (ingredientId, recipeId) VALUES (8, 2);
