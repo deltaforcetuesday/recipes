7fc2e360465385c9440f7994f06c7873c25f11ac
goergena <goergena@users.noreply.github.com>
2018-05-17 12:30:04 -0400

    ingredient model belongstomany recipe

--- models/ingredients.js
+++ models/ingredients.js
@@ -1,50 +1,52 @@
 
 module.exports = function(sequelize, DataTypes) {
-    var Ingredient = sequelize.define("Ingredient", {
-      ingredient: {
+  var Ingredient = sequelize.define("Ingredient", {
+    ingredient: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    /*
+    recipeId: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: "Recipe",
+            key: 'id'
+        }
+    }, */
+    amount: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      len: [1]
+    },
+    measurement: {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
           len: [1]
         }
-      },
-      /*
-      recipeId: {
-          type: DataTypes.INTEGER,
-          references: {
-              model: "Recipe",
-              key: 'id'
-          }
-      }, */
-      amount: {
-        type: DataTypes.DECIMAL,
-        allowNull: false,
-        len: [1]
-      },
-      measurement: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          validate: {
-            len: [1]
-          }
-      },
-    });
-  
-
-    Ingredient.associate = function(models) {
-      Ingredient.belongsTo(models.Recipe, {
-        foreignKey: {
-          allowNull: false
-        }
-      });
-    };
+    },
+  });
 /*
-    Ingredient.associate = function(models) {
-      Ingredient.belongsToMany(models.Recipe, {
-        through: RecipeIngredient
-        });
-    }; */
 
-    return Ingredient;
-  };
-  
+  Ingredient.associate = function(models) {
+    Ingredient.belongsTo(models.Recipe, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  }; */
+
+  Ingredient.associate = function(models) {
+    Ingredient.belongsToMany(models.Recipe, {
+      through: models.RecipeIngredient,
+      foreignKey: 'ingredientId',
+      otherKey: 'recipeId',
+      constraints: false
+      });
+  }; 
+
+  return Ingredient;
+};
