c8a42e0cea713d623b33aed4ef81bd97a95f830c
goergena <goergena@users.noreply.github.com>
2018-05-18 11:08:04 -0400

    share.js renamed functions and variables. more readable

--- public/js/share.js
+++ public/js/share.js
@@ -1,41 +1,46 @@
 $(document).ready(function () {
 
-  $(document).on("click", "#recipeSubmit", addUserBtn)
+  $(document).on("click", "#recipeSubmit", submitRecipe)
   //$("#add-user-btn").on("click", function(){
   //});
 
-  function addUserBtn(event) {
+  function submitRecipe(event) {
     event.preventDefault();
-    /*
-    var username = $("#username-input");
-    var password = $("#password-input");
-    var email = $("#emailAddress");
-    // Don't do anything if the fields are empty
-    if (!username.val().trim() ||
-        !password.val().trim() ||
-        !email.val().trim()) {
-        return;
-    } 
-    //call addCheftoDb and pass in the user inputs
-    addCheftoDb({
-        chefId: $("#chef-name-input").val().trim(),
-        title: $("#recipe-name-input").val().trim(),
-        method: $("#method-input").val().trim(),
-        prepTime: $("#time-input").val().trim()
-    }); */
+    
+    //set all the inputs to variables
+    var ChefId = $("#chef-name-input");
+    var title = $("#recipe-name-input");
+    var method = $("#method-input");
+    var prepTime = $("#time-input");
 
-    addCheftoDb({
-      ChefId: 1,
-      title: 'salsa',
-      method: 'none',
-      prepTime: 10
-    });
+    // Don't do anything if the fields are empty
+    //this is commented out for now for easier testing
+    // if (!ChefId.val().trim() ||
+    //     !title.val().trim() ||
+    //     return;
+    // } 
+    
+    //call addRecipetoDb and pass in the user inputs
+    addRecipetoDb({
+        ChefId: ChefId.val().trim(),
+        title: title.val().trim(),
+        method: method.val().trim(),
+        prepTime: prepTime.val().trim()
+    }); 
+
+//calls addRecipetoDb on button click with dummy data instead of user input.
+    // addRecipetoDb({
+    //   ChefId: 1,  //if there are no chefs in the db, comment out this line. otherwise it will error out
+    //   title: 'salsa',
+    //   method: 'none',
+    //   prepTime: 10
+    // });
   };
 
-  // A function for creating a chef then console logging the info 
-  function addCheftoDb(authorData) {
-    $.post("/api/recipes", authorData)
-      .then(console.log(authorData));
+  // A function for creating a recipe in the database then console logging the info 
+  function addRecipetoDb(submitData) {
+    $.post("/api/recipes", submitData)
+      .then(console.log(submitData));
   }
 
 
