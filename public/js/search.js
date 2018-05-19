99df74f9523bf02aa06cf4a38a65904e8d51d1f2
goergena <goergena@users.noreply.github.com>
2018-05-19 14:09:09 -0400

    search functions and edit recipe api

--- public/js/search.js
+++ public/js/search.js
@@ -1,5 +1,33 @@
 $(document).ready(function () {
- console.log("hey well at least the js file is connected");
+    console.log("hey well at least the js file is connected");
+
+
+    $(document).on("click", "#advSearchBtn", runSearch);
+
+    function runSearch(event) {
+        event.preventDefault();
+
+        var title = $("#recipe-name-input").val().trim();
+        var method = $("#method-input").val().trim();
+        var prepTime = $("#time-input").val().trim();
+
+        var tTitle = "fettucine alfredo";
+        var tMethod = "stove";
+
+        searchRecipe(tTitle);
+
+
+    };
+
+    function searchRecipe(input) {
+        $.get("/api/recipes/?title=" + input, function (data) {
+            console.log("input searched: " + input)
+            console.log("search recipe response: " + JSON.stringify(data))
+        })
+    };
+
+
+
 
 
 });
--- routes/recipe-api.js
+++ routes/recipe-api.js
@@ -1,10 +1,110 @@
 var db = require("../models");
 
+const Op = db.Sequelize.Op;
+
 
 module.exports = function (app) {
 
+  // Post.findAll({
+  //   where: {
+  //     authorId: {
+  //       [Op.or]: [12, 13]
+  //     }
+  //   }
+  // });
 
-  app.get("/api/recipes", function (req, res) {
+  // {
+  //   rank: {
+  //     [Op.or]: {
+  //       [Op.lt]: 1000,
+  //       [Op.eq]: null
+  //     }
+  //   }
+  // }
+  // // rank < 1000 OR rank IS NULL
+
+  // {
+  //   createdAt: {
+  //     [Op.lt]: new Date(),
+  //     [Op.gt]: new Date(new Date() - 24 * 60 * 60 * 1000)
+  //   }
+  // }
+  // // createdAt < [timestamp] AND createdAt > [timestamp]
+
+  // {
+  //   [Op.or]: [
+  //     {
+  //       title: {
+  //         [Op.like]: 'Boat%'
+  //       }
+  //     },
+  //     {
+  //       description: {
+  //         [Op.like]: '%boat%'
+  //       }
+  //     }
+  //   ]
+  // }
+  // // title LIKE 'Boat%' OR description LIKE '%boat%'
+
+  // app.get("/api/recipes", function (req, res) {
+  //   var query = {};
+  //   if (req.query.id) {
+  //     query.id = req.query.id;
+  //   }
+  //   if (req.query.title) {
+  //     query.title = req.query.title;
+  //   }
+  //   // if (req.query.method) {
+  //   //   query.method = req.query.method;
+  //   // }
+  //   // if (req.query.timeInput) {
+  //   //   query.timeInput = req.query.timeInput;
+  //   // }
+  //   db.Recipe.findAll({
+  //     where: {
+  //       [Op.or]: [{
+  //           id: query.id,
+  //         },
+  //         {
+  //           title: query.title
+  //         }
+  //       ]
+  //     },
+  //     include: [db.Chef, db.Ingredient],
+  //   }).then(function (dbRecipe) {
+  //     res.json(dbRecipe);
+  //   });
+  // });
+
+
+    // // title LIKE 'Boat%' OR description LIKE '%boat%'
+
+    app.get("/api/recipes", function (req, res) {
+      var query = {};
+      // if (req.query.id) {
+      //   query.id = req.query.id;
+      // }
+      if (req.query.title) {
+        query.title = req.query.title;
+      }
+      db.Recipe.findAll({
+
+        where: {
+          title: {
+            [Op.or]: [query.title, 'Chicken Curry']
+          }
+         
+        },
+        include: [db.Chef, db.Ingredient],
+      }).then(function (dbRecipe) {
+        res.json(dbRecipe);
+      });
+    });
+
+  /*
+
+   app.get("/api/recipes", function (req, res) {
     var query = {};
     if (req.query.id) {
       query.id = req.query.id
@@ -16,49 +116,48 @@ module.exports = function (app) {
       res.json(dbRecipe);
     });
   });
+    
+    app.get("/api/recipes/:title", function(req, res) {
+        db.Recipe.findAll({
+            where: {
+                title: req.params.title
+            },
+            include: [db.Chef, db.Ingredient],
+        }).then(function(dbRecipe){
+            res.json(dbRecipe);
+        })
+    })  
+
+    
 
 
+    app.get("/api/recipes/:id", function(req, res) {
+      // var query = {};
+      // if (req.query.author_id) {
+      //   query.AuthorId = req.query.author_id;
+      // }
+      db.Recipe.findOne({
+        where: {
+          id: req.params.id
+        },
+        include: [db.Chef, db.Ingredient]
+      }).then(function(dbRecipe) {
+        res.json(dbRecipe);
+      });
+    });  
 
   /*
-  app.get("/api/ingredients/:recipid", function(req, res) {
-      db.Ingredient.findAll({
-          where: {
-              RecipeId: req.params.recipid
-          },
-          include: [db.Recipe],
-      }).then(function(dbIngred){
-          res.json(dbIngred);
-      })
-  })  
+    app.get("/recipe", function(req, res) {
 
-
-  app.get("/api/recipes/:id", function(req, res) {
-    // var query = {};
-    // if (req.query.author_id) {
-    //   query.AuthorId = req.query.author_id;
-    // }
-    db.Recipe.findOne({
-      where: {
-        id: req.params.id
-      },
-      include: [db.Chef, db.Ingredient]
-    }).then(function(dbRecipe) {
-      res.json(dbRecipe);
-    });
-  });  
-
-/*
-  app.get("/recipe", function(req, res) {
-
-    db.Recipe.findOne({
-      where: {
-        id: req.params.id
-      },
-      include: [db.Chef, db.Ingredient]
-    }).then(function(dbRecipe) {
-      res.json(dbRecipe);
-    });
-  }); */
+      db.Recipe.findOne({
+        where: {
+          id: req.params.id
+        },
+        include: [db.Chef, db.Ingredient]
+      }).then(function(dbRecipe) {
+        res.json(dbRecipe);
+      });
+    }); */
 
 
   app.post("/api/recipes", function (req, res) {
