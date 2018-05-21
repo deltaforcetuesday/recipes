95d067e1d5395b503f923cb5d59100259ae45f95
goergena <goergena@users.noreply.github.com>
2018-05-21 13:13:54 -0400

    sharing a recipe works based on login id. removed chefname field from share.html

--- public/js/share.js
+++ public/js/share.js
@@ -1,7 +1,10 @@
 $(document).ready(function () {
 
+  var chefId;
+
   $.get("/api/user_data").then(function (data) {
     $(".member-name").text(data.name);
+    chefId = data.id;
   });
 
   var ingCount = 4;
@@ -48,15 +51,12 @@ $(document).ready(function () {
     ingCount++;
   });
 
-  $(document).on("click", "#recipeSubmit", submitRecipe)
-  //$("#add-user-btn").on("click", function(){
-  //});
+  $(document).on("click", "#recipeSubmit", submitRecipe);
+
 
   function submitRecipe(event) {
     event.preventDefault();
 
-    //set all the inputs to variables
-    var ChefId = $("#chef-name-input");
     var title = $("#recipe-name-input");
     var method = $("#method-input");
     var prepTime = $("#time-input");
@@ -64,54 +64,44 @@ $(document).ready(function () {
 
     // Don't do anything if the fields are empty
     //this is commented out for now for easier testing
-    // if (!ChefId.val().trim() ||
-    //     !title.val().trim() ||
+    // if (!title.val().trim() ||
+    //     !prepTime.val().trim() ||
     //     return;
     // } 
 
     //call addRecipetoDb and pass in the user inputs
     addRecipetoDb({
-      ChefId: ChefId.val().trim(),
+      ChefId: chefId,
       title: title.val().trim(),
       method: method.val().trim(),
       prepTime: prepTime.val().trim(),
       instructions: instructions.val().trim(),
     });
 
-    function addIngredienttoDb(data) {
-      $.post("/api/ingredients", data)
-        .then(console.log(data))
-    }
-
-    // A function for creating a recipe in the database then console logging the info 
-    function addRecipetoDb(submitData) {
-      $.post("/api/recipes", submitData)
-        .then(function (res) {
-          console.log(res);
-
-
-          // var allIng = [];
-          for (let i = 1; i < ingCount; i++) {
-            var newIngredient = {
-              ingredient: $("#ingredient-" + i).val().trim(),
-              amount: $("#amount-" + i).val(),
-              measurement: $("#measurement-" + i).val().trim(),
-              RecipeId: res['id']
-            }
-            addIngredienttoDb(newIngredient);
-            // allIng.push(newIngredient);
-          }
-
-        });
-    }
-    // console.log("this is the array" + allIng);
-    // console.log(allIng);  
   };
+
+  function addIngredienttoDb(data) {
+    $.post("/api/ingredients", data)
+      .then(console.log(data))
+  }
+
+
+  function addRecipetoDb(submitData) {
+    $.post("/api/recipes", submitData)
+      .then(function (res) {
+        console.log(res);
+
+        for (let i = 1; i < ingCount; i++) {
+          var newIngredient = {
+            ingredient: $("#ingredient-" + i).val().trim(),
+            amount: $("#amount-" + i).val(),
+            measurement: $("#measurement-" + i).val().trim(),
+            RecipeId: res['id']
+          }
+          addIngredienttoDb(newIngredient);
+        }
+
+      });
+  }
+
 });
-//calls addRecipetoDb on button click with dummy data instead of user input.
-// addRecipetoDb({
-//   ChefId: 1,  //if there are no chefs in the db, comment out this line. otherwise it will error out
-//   title: 'salsa',
-//   method: 'none',
-//   prepTime: 10
-// });
